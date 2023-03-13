68bddc46f367a4d4e961bd1c501b144e346e32c7
Tolokoban <contact@tolokoban.org>
2023-03-13 21:40:21 +0100

    Try to make this work without error on colord is missing

--- doc/webpack.config.js
+++ doc/webpack.config.js
@@ -19,164 +19,184 @@ if (typeof package.port !== "number") {
     console.log("A random port has been set for dev server:", package.port)
 }
 
-module.exports = {
-    cache: {
-        type: "memory",
-    },
-    output: {
-        filename: "./scr/[name].[contenthash].js",
-        path: Path.resolve(__dirname, "build"),
-        assetModuleFilename: "asset/[name].[hash][ext][query]",
-    },
-    entry: {
-        app: "./src/index.tsx",
-    },
-    target: "web",
-    resolve: {
-        extensions: [".tsx", ".ts", ".js", ".jsx", ".wasm"],
-        enforceExtension: false,
-        alias: {
-            "@": Path.resolve(__dirname, "src"),
-            react: require.resolve("react"),
+module.exports = (env) => {
+    const isProdMode = env.WEBPACK_BUILD === true
+    const isTestMode = !isProdMode
+    if (isProdMode) {
+        console.log("+-----------------+")
+        console.log("| Production Mode |")
+        console.log("+-----------------+")
+    }
+    return {
+        cache: {
+            type: "memory",
         },
-    },
-    devtool: "inline-source-map",
-    devServer: {
-        compress: true,
-        static: {
-            directory: Path.resolve(__dirname, "./public"),
+        output: {
+            filename: "./scr/[name].[contenthash].js",
+            path: Path.resolve(__dirname, "build"),
+            assetModuleFilename: "asset/[name].[hash][ext][query]",
         },
-        client: {
-            logging: "verbose",
-            overlay: { errors: true, warnings: false },
-            progress: true,
+        entry: {
+            app: "./src/index.tsx",
         },
-        hot: true,
-        // Open WebBrowser.
-        open: true,
-        port: process.env.PORT || package.port,
-    },
-    plugins: [
-        new CleanWebpackPlugin({
-            // We don't want to remove the "index.html" file
-            // after the incremental build triggered by watch.
-            cleanStaleWebpackAssets: false,
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: Path.resolve(__dirname, "public"),
-                    filter: async (path) => {
-                        return !path.endsWith("index.html")
+        target: "web",
+        resolve: {
+            extensions: [".tsx", ".ts", ".js", ".jsx", ".wasm"],
+            enforceExtension: false,
+            alias: {
+                "@tolokoban/ui": Path.resolve(__dirname, "../lib/src"),
+                "@": Path.resolve(__dirname, "src"),
+                // react: require.resolve("react"),
+                // colord: require.resolve("colord"),
+            },
+        },
+        devtool: "inline-source-map",
+        devServer: {
+            compress: true,
+            static: {
+                directory: Path.resolve(__dirname, "./public"),
+            },
+            client: {
+                logging: "verbose",
+                overlay: { errors: true, warnings: false },
+                progress: true,
+            },
+            hot: true,
+            // Open WebBrowser.
+            open: true,
+            port: process.env.PORT || package.port,
+        },
+        plugins: [
+            new CleanWebpackPlugin({
+                // We don't want to remove the "index.html" file
+                // after the incremental build triggered by watch.
+                cleanStaleWebpackAssets: false,
+            }),
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: Path.resolve(__dirname, "public"),
+                        filter: async (path) => {
+                            return !path.endsWith("index.html")
+                        },
                     },
+                ],
+            }),
+            new HtmlWebpackPlugin({
+                meta: {
+                    viewport:
+                        "width=device-width, initial-scale=1, shrink-to-fit=no",
+                    "theme-color": "#56abff",
+                },
+                template: "public/index.html",
+                filename: "index.html",
+                title: package.name,
+                version: package.version,
+                minify: {
+                    collapseInlineTagWhitespace: true,
+                    collapseWhitespace: true,
+                    decodeEntities: true,
+                    minifyCSS: true,
+                    removeComments: true,
+                },
+            }),
+            new WorkboxPlugin.GenerateSW({
+                // These options encourage the ServiceWorkers to get in there fast
+                // and not allow any straggling "old" SWs to hang around.
+                clientsClaim: true,
+                skipWaiting: true,
+            }),
+        ],
+        optimization: {
+            // Create a single runtime bundle for all chunks.
+            runtimeChunk: "single",
+            splitChunks: {
+                // All the node_modules libs on a single file.
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: "libs",
+                        chunks: "all",
+                    },
+                },
+            },
+            // Prevent "libs.[contenthash].js" from changing its hash if not needed.
+            moduleIds: "deterministic",
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: [
+                        {
+                            loader: "ts-loader",
+                            options: {
+                                transpileOnly: false,
+                            },
+                        },
+                    ],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        {
+                            loader: devMode
+                                ? "style-loader"
+                                : MiniCssExtractPlugin,
+                            options: {
+                                injectType: "styleTag",
+                            },
+                        },
+                        {
+                            loader: "css-loader",
+                            options: {
+                                modules: {
+                                    auto: true,
+                                    localIdentName: isTestMode
+                                        ? "[path][name]_[local]_[hash:base64:6]"
+                                        : "[hash:base64]",
+                                },
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.mdx?$/,
+                    use: [
+                        // `babel-loader` is optional:
+                        { loader: "babel-loader", options: {} },
+                        {
+                            loader: "@mdx-js/loader",
+                            /** @type {import('@mdx-js/loader').Options} */
+                            options: {
+                                /* jsxImportSource: …, otherOptions… */
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.(png|jpe?g|gif|webp|svg)$/i,
+                    // More information here https://webpack.js.org/guides/asset-modules/
+                    type: "asset",
+                    generator: {
+                        filename: "./img/[name].[hash][ext][query]",
+                    },
+                },
+                {
+                    test: /\.(eot|ttf|woff|woff2)$/i,
+                    // More information here https://webpack.js.org/guides/asset-modules/
+                    type: "asset/resource",
+                    generator: {
+                        filename: "./fnt/[name].[hash][ext][query]",
+                    },
+                },
+                {
+                    test: /\.ya?ml$/,
+                    type: "json",
+                    use: "yaml-loader",
                 },
             ],
-        }),
-        new HtmlWebpackPlugin({
-            meta: {
-                viewport:
-                    "width=device-width, initial-scale=1, shrink-to-fit=no",
-                "theme-color": "#56abff",
-            },
-            template: "public/index.html",
-            filename: "index.html",
-            title: package.name,
-            version: package.version,
-            minify: {
-                collapseInlineTagWhitespace: true,
-                collapseWhitespace: true,
-                decodeEntities: true,
-                minifyCSS: true,
-                removeComments: true,
-            },
-        }),
-        new WorkboxPlugin.GenerateSW({
-            // These options encourage the ServiceWorkers to get in there fast
-            // and not allow any straggling "old" SWs to hang around.
-            clientsClaim: true,
-            skipWaiting: true,
-        }),
-    ],
-    optimization: {
-        // Create a single runtime bundle for all chunks.
-        runtimeChunk: "single",
-        splitChunks: {
-            // All the node_modules libs on a single file.
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "libs",
-                    chunks: "all",
-                },
-            },
         },
-        // Prevent "libs.[contenthash].js" from changing its hash if not needed.
-        moduleIds: "deterministic",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: [
-                    {
-                        loader: "ts-loader",
-                        options: {
-                            transpileOnly: false,
-                        },
-                    },
-                ],
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: devMode ? "style-loader" : MiniCssExtractPlugin,
-                        options: {
-                            injectType: "styleTag",
-                        },
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {},
-                    },
-                ],
-            },
-            {
-                test: /\.mdx?$/,
-                use: [
-                    // `babel-loader` is optional:
-                    { loader: "babel-loader", options: {} },
-                    {
-                        loader: "@mdx-js/loader",
-                        /** @type {import('@mdx-js/loader').Options} */
-                        options: {
-                            /* jsxImportSource: …, otherOptions… */
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif|webp|svg)$/i,
-                // More information here https://webpack.js.org/guides/asset-modules/
-                type: "asset",
-                generator: {
-                    filename: "./img/[name].[hash][ext][query]",
-                },
-            },
-            {
-                test: /\.(eot|ttf|woff|woff2)$/i,
-                // More information here https://webpack.js.org/guides/asset-modules/
-                type: "asset/resource",
-                generator: {
-                    filename: "./fnt/[name].[hash][ext][query]",
-                },
-            },
-            {
-                test: /\.ya?ml$/,
-                type: "json",
-                use: "yaml-loader",
-            },
-        ],
-    },
+    }
 }
