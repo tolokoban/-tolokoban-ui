bde82820b4b464ff04be9b09dd767ad01c8f2580
Tolokoban <contact@tolokoban.org>
2024-06-04 13:15:56 +0200

    v0.17.1

--- doc/src/info.ts
+++ doc/src/info.ts
@@ -1,4 +1,4 @@
 export const name = "@tolokoban/ui"
-export const version = "0.17.0"
+export const version = "0.17.1"
 export const description = "React components with theme"
 export const homepage = "https://tolokoban.github.io/ui"
--- lib/src/view/index.ts
+++ lib/src/view/index.ts
@@ -1,4 +1,4 @@
-// This file has been generated on Tue, 04 Jun 2024 09:55:33 GMT
+// This file has been generated on Tue, 04 Jun 2024 11:15:23 GMT
 export * from "./Button"
 export * from "./Chip"
 export * from "./Combo"
