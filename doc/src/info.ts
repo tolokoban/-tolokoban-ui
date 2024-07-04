2a176fe082d768a0be8c2f4ff4cc72410132f148
Tolokoban <contact@tolokoban.org>
2024-07-04 09:41:15 +0200

    Dependencies upgrade

--- doc/src/info.ts
+++ doc/src/info.ts
@@ -1,4 +1,4 @@
 export const name = "@tolokoban/ui"
-export const version = "0.17.8"
+export const version = "0.17.9"
 export const description = "React components with theme"
 export const homepage = "https://tolokoban.github.io/ui"
