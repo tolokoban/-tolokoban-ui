a4744e8073f0b74fe2060d40ea7b4abf48db0b13
Tolokoban <contact@tolokoban.org>
2024-11-18 09:40:02 +0100

    upgrade licenses

--- lib/src/info.ts
+++ lib/src/info.ts
@@ -1,4 +1,4 @@
 export const name = "@tolokoban/ui"
-export const version = "0.20.2"
+export const version = "0.20.3"
 export const description = "React components with theme"
 export const homepage = "https://tolokoban.github.io/ui"
