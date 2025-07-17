ccc43e5e3e4e37552ef9d7ef09d7e7719d7c512e
Tolokoban <contact@tolokoban.org>
2025-07-17 16:45:07 +0200

    Fix tooltip positionning

--- lib/src/info.ts
+++ lib/src/info.ts
@@ -1,4 +1,4 @@
 export const name = "@tolokoban/ui"
-export const version = "0.22.0"
+export const version = "0.22.1"
 export const description = "React components with theme"
 export const homepage = "https://tolokoban.github.io/ui"
